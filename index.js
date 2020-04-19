35ec0ebb71ab17b4108fde14f898d9b7d6f44d9a
ga-devfront <root@GameComputer.localdomain>
2020-04-19 15:44:09 +0200

    ✨ add calcul for scroll by navigator

--- index.js
+++ index.js
@@ -1,45 +1,76 @@
 export default (settings) => {
-    const userAgent = window.navigator.userAgent;
-    const regex = {
-        firefox: /Firefox[\/\s](\d+\.\d+)/,
-        ie: /MSIE (\d+\.\d+);/,
-        edge: /Edge[\/\s](\d+\.\d+)/,
-        chrome: /\(KHTML, like Gecko\) Chrome/,
-        opera: /OPR[\/\s](\d+\.\d+)/,
-    }
-
-    let navigator = null;
-
-    if (regex.firefox.test(userAgent)) {
-       navigator = 'firefox';
-    }
-
-    if (regex.ie.test(userAgent)) {
-        navigator = 'ie';
-    }
-
-    if (regex.edge.test(userAgent)) {
-        navigator = 'edge';
-    }
-
-    if (regex.chrome.test(userAgent) && !regex.opera.test(userAgent) && !regex.edge.test(userAgent)) {
-        navigator = 'chrome';
-    }
-
-    if (regex.opera.test(userAgent)) {
-        navigator = 'opera';
-    }
-
-    if (navigator !== null) {
-        console.log(navigator);
-    }
-
     const container = document.querySelector(settings.container);
+    const userAgent = window.navigator.userAgent;
+    console.log(userAgent);
+    const navigators = {
+        chrome: {
+            name: 'chrome',
+            regex: /\(KHTML, like Gecko\) Chrome/,
+            deltaY: 100
+        },
+        edge: {
+            name: 'edge',
+            regex: /Edge[\/\s](\d+\.\d+)/,
+            deltaY: 145.0500030517578
+        },
+        firefox: {
+            name: 'firefox',
+            regex: /Firefox[\/\s](\d+\.\d+)/,
+            deltaY: 3
+        },
+        ie: {
+            name: 'ie',
+            regex: /MSIE (\d+\.\d+);/,
+            deltaY: 0
+        },
+        opera: {
+            name: 'opera',
+            regex: /OPR[\/\s](\d+\.\d+)/,
+            deltaY: 100
+        },
+    }
 
-    container.addEventListener('wheel', (element) => {
-        console.log(element);
-        element.preventDefault();
-        element.stopPropagation();
-    });
+    let currentNavigator = null;
+    let scrollCount = 0;
+
+    if (navigators.chrome.regex.test(userAgent) && !navigators.opera.regex.test(userAgent) && !navigators.edge.regex.test(userAgent)) {
+        currentNavigator = navigators.chrome;
+    }
+
+    if (navigators.edge.regex.test(userAgent)) {
+        currentNavigator = navigators.edge;
+    }
+
+    if (navigators.firefox.regex.test(userAgent)) {
+        currentNavigator = navigators.firefox;
+    }
+
+    if (navigators.ie.regex.test(userAgent)) {
+        currentNavigator = navigators.ie;
+    }
+
+    if (navigators.opera.regex.test(userAgent)) {
+        currentNavigator = navigators.opera;
+    }
+
+    // console.log test
+    if (currentNavigator !== null) {
+        console.log(currentNavigator);
+    }
+
+    if (currentNavigator === null) {
+        console.log('sorry but scroll listener do not run on this navigator');
+        return
+    }
+
+    if (currentNavigator.name === 'chrome' || 'edge' || 'firefox' || 'ie' || 'opera') {
+        container.addEventListener('wheel', (element) => {
+            element.preventDefault();
+            element.stopPropagation();
+            const calcForOneScroll = element.deltaY / currentNavigator.deltaY;
+            scrollCount += calcForOneScroll;
+            console.log(scrollCount);
+        });
+    }
 
 }
